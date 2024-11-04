fed1832ed156bd6f73d3fc69a4fc2cfcc7b53390
danielreg607 <116653986+danielreg607@users.noreply.github.com>
2024-11-03 21:15:39 -0600

    Update CalculadoraServer.java

--- CalculadoraServer.java
+++ CalculadoraServer.java
@@ -6,6 +6,7 @@ public class CalculadoraServer {
 	public CalculadoraServer() {
 		try {
 			System.setSecurityManager(new SecurityManager());
+			System.setProperty("java.rmi.server.hostname","10.86.15.199");
 			Calculadora c = new CalculadoraObj();
 			Naming.rebind("//localhost/ServicioDeCalculadora", c);
 			System.out.println("Servidor listo...");
